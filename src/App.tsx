c112389fa1b32583c12e43e9372ae27cea924cda
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-10-08 14:47:40 -0300

    Implementação da captura de imagem do vídeo selecionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [urlVideo, setUrlVideo] = useState("");
+    const [urlImagem, setUrlImagem] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function aoSelecionarArquivo(event: ChangeEvent<HTMLInputElement>): void {
+        const arquivo = event.target.files?.[0];
+        if (arquivo) {
+            const url = URL.createObjectURL(arquivo);
+            setUrlVideo(url);
+        }
+    }
+
+    function aoCapturarImagem(): void {
+        const canvas = document.createElement("canvas");
+        const video = document.querySelector("video");
+        canvas.width = video?.videoWidth || 0;
+        canvas.height = video?.videoHeight || 0;
+        const contexto = canvas.getContext("2d");
+        contexto?.drawImage(
+            video as HTMLVideoElement,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+        const url = canvas.toDataURL("image/png");
+        setUrlImagem(url);
+    }
+
+    return (
+        <>
+            <div className="container">
+                <video src={urlVideo} controls width="500px" height="300px">
+                    Seu browser não suporta a tag video
+                </video>
+                <input
+                    type="file"
+                    id="arquivo"
+                    className="mt-3"
+                    accept="video/*"
+                    onChange={aoSelecionarArquivo}
+                />
+                <button
+                    className="btn btn-primary mt-3"
+                    onClick={aoCapturarImagem}
+                >
+                    Capturar imagem
+                </button>
+                {urlImagem && (
+                    <img
+                        src={urlImagem}
+                        alt="Captura de tela"
+                        className="mt-3"
+                        width={"320px"}
+                        height={"auto"}
+                    />
+                )}
+            </div>
+        </>
+    );
 }
-
-export default App
+export default App;
